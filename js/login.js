2c80378201ab003df6c0a0e6fd2f58cd8deb03ee
Gabriel Ramos <154455595+GGabriel7@users.noreply.github.com>
2024-07-01 11:38:38 -0300

    Tela de Login
    Criação da tela de login

---
+++ js/login.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('.login-inp')
+const button = document.querySelector('.login-but')
+const form = document.querySelector('.login')
+
+//Validar o nome do usuário
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled')
+        return
+    }
+
+    button.setAttribute('disabled', '')
+}
+
+const ativarSubmit = (event) => {
+    event.preventDefault()
+
+    localStorage.setItem('player', input.value)
+    window.location = '../pages/game.html'
+}
+
+input.addEventListener('input', validateInput) // Colando o evento de validação no login-ino
+form.addEventListener('submit', ativarSubmit)
