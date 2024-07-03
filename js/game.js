bc563275608525a8af2d98344915dc5e094d121d
Gabriel Ramos <154455595+GGabriel7@users.noreply.github.com>
2024-07-03 11:29:58 -0300

    Primeira versão jogavel
    Funcionalidade de virada da carta está funcionando.

--- js/game.js
+++ js/game.js
@@ -21,6 +21,62 @@ const criarElemento = (tag, classNome) => {
     return element
 }
 
+//varias para as duas cartas que serão viradas
+let card1 = ''
+let card2 = ''
+
+//Função para o fim do jogo
+const endGame = () => {
+    const cardDesabilit = document.querySelectorAll('.cardDesabilit')
+
+    if (cardDesabilit.length === 20) {
+        alert('Parabens! Você terminou')
+    }
+}
+
+//Checar se as Cartas já foram viradas.
+const checarCard = () => {
+    const personagem1 = card1.getAttribute('data-personagem')
+    const personagem2 = card2.getAttribute('data-personagem')
+
+    if (personagem1 === personagem2) {
+        setTimeout(() => {   
+            card1.firstChild.classList.add('cardDesabilit')
+            card2.firstChild.classList.add('cardDesabilit')
+            card1 = ''
+            card2 = ''
+        }, 500)
+        
+        endGame()
+    } else {
+      setTimeout(() => {
+        card1.classList.remove('revelarCard')
+        card2.classList.remove('revelarCard')
+
+        card1 = ''
+        card2 = ''
+      }, 1000)
+    }
+}
+
+//Função para revelar carta
+const revelarCard = ({target}) => {
+    if (target.parentNode.className.includes('revelarCard')) {
+        return
+    }
+
+    if (card1 === '') {
+        target.parentNode.classList.add('revelarCard')
+        card1 = target.parentNode
+    } else if (card2 === '') {
+        target.parentNode.classList.add('revelarCard')
+        card2 = target.parentNode
+
+        checarCard()
+    }
+    
+}
+
 // Para criar o Card
 const criarCard = (personagem) => {
     const card = criarElemento('div', 'card')
@@ -32,10 +88,14 @@ const criarCard = (personagem) => {
     card.appendChild(front)
     card.appendChild(back)
 
+    card.addEventListener('click', revelarCard)
+
+    card.setAttribute('data-personagem', personagem)
+
     return card
 }
 
-//Função para criar as cartas
+//Função para colocar os personagens nas cartas
 const loadGame = () => {
     const duplicarPersonagens = [...personagens, ...personagens]
 
